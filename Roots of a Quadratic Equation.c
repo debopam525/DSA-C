e69e1179df8aed872f5ce2479b77681d98d3dfd3
debopam525 <debopam525@GMAIL.com>
2024-12-27 12:38:36 +0530

    Finding the real roots of a Quadratic Equation

---
+++ Roots of a Quadratic Equation.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+main()
+{
+    float a,b,c,d,x1,x2;
+    printf("a= ");
+    scanf("%f", &a);
+    printf("b= ");
+    scanf("%f", &b);
+    printf("c= ");
+    scanf("%f", &c);
+    d=sqrt(b*b-4*a*c);
+    x1=(-b+d)/(2*a);
+    x2=(-b-d)/(2*a);
+    printf("\nx1 = %e    x2 = %e",x1,x2);
+}
