1d22462d5c52723a989efedaae0e7a997542416d
debopam525 <debopam525@GMAIL.com>
2024-12-27 11:38:57 +0530

    Use of flags with integer and floating-point numbers

---
+++ Use of flags.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+main()
+{
+    int i=123;
+    float x=12.0,y=-3.3;
+    printf(":%6d %7.0f %10.1e:\n\n", i,x,y);
+    printf(":%-6d %-7.0f %-10.1e:\n\n", i,x,y);
+    printf(":%+6d %+7.0f %+10.1e:\n\n", i,x,y);
+    printf(":%-+6d %-+7.0f %-+10.1e:\n\n", i,x,y);
+    printf(":%7.0f %#7.0f %7g %#7g:",x,x,y,y);
+}
